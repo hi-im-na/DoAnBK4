3ee026ff1980e9a4f11a1c83aee54560ce301d81
pinkyhero00 <46052990+pinkyhero00@users.noreply.github.com>
2020-07-01 18:54:57 +0700

    +delete

--- target/classes/static/js/check.js
+++ target/classes/static/js/check.js
@@ -13,18 +13,23 @@
 //         // return false;
 //     }
 // }
+
+$(document).ready(function () {
+  $(".toast").toast('show');
+})
+
 function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML =
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
     h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-  }
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+  return i;
+}
